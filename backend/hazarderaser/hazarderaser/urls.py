f2747659592e7f65dee0c4a77bc5101ae793562b
salexkidd <salexkidd@gmail.com>
2014-02-08 16:43:20 +0900

    fix urls.py

--- backend/hazarderaser/hazarderaser/urls.py
+++ backend/hazarderaser/hazarderaser/urls.py
@@ -1,12 +1,15 @@
+# -*- coding:utf-8 -*-
+"""
+hazarderaser urls module
+"""
+#django modules
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'hazarderaser.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hazard/', include('hazard.urls')),
 )
